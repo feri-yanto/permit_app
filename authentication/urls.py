c98d6c1e8620eda3f39632916c8f1af3f45a4c39
feri-yanto <feriiiyanto.i7@gmail.com>
2025-08-13 13:47:20 +0800

    add new url path

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import RegisterUserView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
+from .views import RegisterUserView, LoginUserView, register_success_view, confirm_user
 
 app_name = 'authentication'
 urlpatterns = [
    path('register', view=RegisterUserView.as_view(), name='register'),
+   path('register/success', view=register_success_view, name='success'),
+   path('login', view=LoginUserView.as_view(), name='login'),
+   path('logout', view=login_required(function=LogoutView.as_view(), login_url='/auth/login'), name='logout'),
+   path('validation/<str:id>/<str:token>', view=confirm_user, name='validation')
 ]
