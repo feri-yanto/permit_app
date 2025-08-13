91d6da66ccf307d0607fd31971d49a16e6af3e9c
feri-yanto <feriiiyanto.i7@gmail.com>
2025-08-13 13:46:05 +0800

    add LoginForm and customize save method

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
 from .models import Accounts
 
+
+# REGISTER FORM
 class RegisterForm(forms.ModelForm):
    password = forms.CharField(min_length=8, required=True, validators=[
       RegexValidator(regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$', message=['Sandi minimal 8 karakter', 'Terdapat huruf kapital', 'Terdapat simbol (!@#$%^&*)'], code='invalid')
    ], widget=forms.PasswordInput())
+   
    class Meta:
       model = Accounts
       fields = ['username', 'email', 'password']
@@ -15,9 +20,31 @@ class RegisterForm(forms.ModelForm):
          'email': {'unique': _('Email telah digunakan')},
       }
    
-   def save(self, commit = False):
+   def save(self, commit = True):
       password = self.cleaned_data['password']
-      instance = super().save(commit)
-      instance.set_password(password)
-      instance.save()
-      return instance
+      self.instance.is_active = False
+      self.instance.set_password(password)
+      return super().save(commit)
+
+
+# LOGIN FORM
+class LoginForm(forms.Form):
+   username = forms.CharField(max_length=50, required=True, widget=forms.TextInput())
+   password = forms.CharField(min_length=8, required=True, widget=forms.PasswordInput())
+
+   def __init__(self, request, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.request = request
+      self.__user = None
+   
+   @property
+   def get_user(self):
+      return self.__user
+   
+   def clean(self):
+      data = self.cleaned_data
+      user = authenticate(self.request, **data)
+      if user is None:
+         raise ValidationError(message=_('Login gagal, pastikan akun anda telah aktif dan data yang anda masukan benar'), code='login_invalid')
+      self.__user = user
+      return super().clean()
